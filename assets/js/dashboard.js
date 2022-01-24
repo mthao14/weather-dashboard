2d627d12ce4352544c0731ddf2b0a46e76cd6c63
you@example.com <mthao14@outlook.com>
2022-01-23 23:04:49 -0600

    updated api

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,6 +1,13 @@
 var getWeather = function () {
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=43.04&lon=-87.91&appid=a2acb9c16d4c5fc551b580aca838cb08");
-    console.log(data);
-}
+
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=name&timezone&main.temp&wind&main.humidity&limit=5&appid=a2acb9c16d4c5fc551b580aca838cb08";
+
+    fetch(apiUrl).then(function(data) {
+        console.log(data);
+    });
+};
+
+console.log("outside");
+
 
 getWeather();
